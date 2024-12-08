0dc8d8985b7792c292e00a13616d44860a94844b
arinvasc <arinvascript@gmail.com>
2024-12-08 17:20:52 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-120x120.png',
+  '/icon-152x152.png',
+  '/icon-180x180.png',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      // Offline fallback
+      if (event.request.mode === 'navigate') {
+        return caches.match('/index.html');
+      }
+    })
+  );
+});
